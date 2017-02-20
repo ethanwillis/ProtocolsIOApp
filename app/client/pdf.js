a7755ca84667110bde129f91d4c4b23735287a9f
Matthew Longley <randomticktock@gmail.com>
2017-02-20 12:52:32 -0600

    added support for displaying pdfs

---
+++ app/client/pdf.js
@@ -0,0 +1,43 @@
+require('pdfjs-dist');
+PDFJS.workerSrc = 'http://localhost:3008/pdf.worker.min.js';
+
+Template.pdf.helpers({});
+Template.pdf.onRendered(function() {
+	var protocolid = FlowRouter.getParam('protocol_id');
+	console.log(protocolid);
+	Meteor.call('get_protocol_pdf', protocolid, function(error, result) {
+		if(error) {
+			alert(error);
+		} else {
+			var loadingTask = PDFJS.getDocument({data: result});
+			loadingTask.promise.then(function(pdf) {
+				console.log('PDF loaded');
+				
+				var pageNumber = 1;
+				pdf.getPage(pageNumber).then(function(page) {
+					console.log('Page loaded');
+					
+					var scale = 1.5;
+					var viewport = page.getViewport(scale);
+					
+					var canvas = document.getElementById('the-canvas');
+					var context = canvas.getContext('2d');
+					
+					canvas.height = viewport.height;
+					canvas.width = viewport.width;
+					
+					var renderContext = {
+						canvasContext: context,
+						viewport: viewport
+					};
+					var renderTask = page.render(renderContext);
+					renderTask.then(function () {
+						console.log('Page rendered');
+					});
+				});
+			}, function(reason) {
+				console.error(reason);
+			});
+		}
+	});
+});
